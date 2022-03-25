a30b9a6224e25626f7d388b30367dee94ea09047
bambooGHT <ras257458@163.com>
2022-03-26 02:44:42 +0800

    代码更新

---
+++ enemy/input.js
@@ -0,0 +1,12 @@
+$('input:radio').click(function () {
+    var domName = $(this).attr('name');
+    var $radio = $(this);
+    if ($radio.data('waschecked') == true) {
+        $radio.prop('checked', false);
+        $("input:radio[name='" + domName + "']").data('waschecked', false);
+    } else {
+        $radio.prop('checked', true);
+        $("input:radio[name='" + domName + "']").data('waschecked', false);
+        $radio.data('waschecked', true);
+    }
+});

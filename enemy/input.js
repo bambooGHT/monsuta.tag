cbc7a33adc7cf2102497e2b04f5e5146f5aa1040
bambooGHT <ras257458@163.com>
2022-04-03 02:41:01 +0800

    更新

--- enemy/input.js
+++ enemy/input.js
@@ -10,3 +10,17 @@ $('input:radio').click(function () {
         $radio.data('waschecked', true);
     }
 });
+
+$(function () {
+    $('.enemy a').on('click', function() {
+        var _ = $(this);
+        var url = _.attr('href');
+
+        //点击a跳转页面
+        location.href = url;
+
+        //添加背景色 css设置a.on { background: #09e; color: #fff; }
+        _.addClass('on').siblings('a').removeClass('on');
+        return false;
+    });
+});
